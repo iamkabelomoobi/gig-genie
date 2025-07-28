f2a9eb25579d27986db913b36ad722b5d50e70f2
Kabelo Moobi <giftmoobi@gmail.com>
2025-07-28 12:56:33 +0200

    ✨ feat: add enums for admin roles, application statuses, job types, and user roles

---
+++ shared/src/enums/admin-role.enum.ts
@@ -0,0 +1,5 @@
+export enum AdminType {
+  SUPER = 'SUPER',
+  MODERATOR = 'MODERATOR',
+  SUPPORT = 'SUPPORT',
+}
---
+++ shared/src/enums/index.ts
@@ -0,0 +1,4 @@
+export { AdminType } from './admin-role.enum.js';
+export { ApplicationStatus } from './application-status.enum.js';
+export { JobType } from './job-type.enum.js';
+export { UserRole } from './user-role.enum.js';
